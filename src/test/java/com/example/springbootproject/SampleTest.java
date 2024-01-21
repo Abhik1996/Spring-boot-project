ea8427c206dd6888ab5fea7a2f28bb285cd3c249
Abhik1996 <patelabhik1996@gmail.com>
2024-01-21 11:01:43 +0000

    dockerfile

---
+++ src/test/java/com/example/springbootproject/SampleTest.java
@@ -0,0 +1,11 @@
+package com.example.springbootproject;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class SampleTest {
+    @Test
+    void sampleTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
